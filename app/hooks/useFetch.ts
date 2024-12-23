6a527aa5fc4092344fcba0e2421d353b9a7e9b17
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2024-12-23 15:53:00 +0300

    Work on categories

---
+++ app/hooks/useFetch.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const fetchData = async () => {
+      try {
+        const resp = await fetch(url);
+        const json = await resp.json();
+
+        if (resp.ok) {
+          setData(json);
+        }
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
--- app/recipes/Categories.tsx
+++ app/recipes/Categories.tsx
@@ -1,33 +1,31 @@
+"use client";
+
+import useFetch from "../hooks/useFetch";
+
+type Category = {
+  idCategory: string;
+  strCategory: string;
+  strCategoryThumb: string;
+};
+
 const Categories = () => {
+const {data, loading, error} = useFetch<{categories: Category[]}>("https://www.themealdb.com/api/json/v1/1/categories.php") 
+
+console.log(data)
+
   return (
     <div className="mt-5 grid grid-cols-3 gap-5">
-      <div className="border border-orange-500 rounded-lg">
-        <img
-          src="https://www.themealdb.com/images/category/beef.png"
-          alt="The meal"
-        />
-        <div className="p-2 bg-white/40 text-center">
-            <span>Beef</span>
+      {data?.categories?.map((category) => (
+        <div
+          key={category.idCategory}
+          className="border border-orange-500 rounded-lg"
+        >
+          <img src={category.strCategoryThumb} alt="The meal" />
+          <div className="p-2 bg-white/40 text-center">
+            <span>{category.strCategory}</span>
+          </div>
         </div>
-      </div>
-      <div className="border border-orange-500 rounded-lg">
-        <img
-          src="https://www.themealdb.com/images/category/beef.png"
-          alt="The meal"
-        />
-        <div className="p-2 bg-white/40 text-center">
-            <span>Beef</span>
-        </div>
-      </div>
-      <div className="border border-orange-500 rounded-lg">
-        <img
-          src="https://www.themealdb.com/images/category/beef.png"
-          alt="The meal"
-        />
-        <div className="p-2 bg-white/40 text-center">
-            <span>Beef</span>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
