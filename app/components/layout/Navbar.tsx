a8799f6675a0bd11a879e380930be0c9cebadc4f
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2024-12-23 14:03:57 +0300

    Make Navbar responisve

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -2,9 +2,10 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [navIsOpen, setnavIsOpen] = useState(false);
   const pathname = usePathname();
 
   return (
@@ -15,7 +16,11 @@ const Navbar = () => {
             Foodie App
           </div>
         </Link>
-        <ul className="flex items-center gap-20 text-xl">
+        <ul
+          className={`flex items-center gap-20 max-md:gap-10 text-xl max-sm:absolute max-sm:left-0 max-sm:right-0 max-sm:flex-col max-sm:top-16 max-sm:bg-white max-sm:py-10 max-sm:z-20 transition origin-top-right ${
+            navIsOpen ? "" : "scale-0"
+          }`}
+        >
           <li
             className={`hover:text-orange-500 transition ${
               pathname === "/" ? "text-orange-500" : ""
@@ -38,6 +43,20 @@ const Navbar = () => {
             <Link href="/favourites">Favourites</Link>
           </li>
         </ul>
+        <div className="sm:hidden">
+          <button
+            className={`${navIsOpen ? "hidden" : ""}`}
+            onClick={() => setnavIsOpen(true)}
+          >
+            Open
+          </button>
+          <button
+            className={`${navIsOpen ? "" : "hidden"}`}
+            onClick={() => setnavIsOpen(false)}
+          >
+            Close
+          </button>
+        </div>
       </nav>
     </header>
   );
