54c3e8cf3d7a0abaf6e2176da1a417c396946a95
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2025-01-10 13:19:01 +0300

    Implement display singleMeal logic

--- app/recipes/[category]/[singleMeal]/page.tsx
+++ app/recipes/[category]/[singleMeal]/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import useFetch from "@/app/hooks/useFetch";
 import { useParams } from "next/navigation";
+// Hooks
+import useFetch from "@/app/hooks/useFetch";
+// Components
+import Error from "@/app/components/ui/Error";
 
 type Meal = {
   idMeal: string;
   strMeal: string;
   strMealThumb: string;
+  strInstructions: string;
+  [key: string]: string;
 };
 
 const page = () => {
@@ -15,32 +20,56 @@ const page = () => {
     `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
 `,
   );
-  console.log(params);
+  const meal = data?.meals[0];
+  console.log("meal:", meal);
+
+  const ingredients: string[] = [];
+
+  if (meal) {
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = meal[`strIngredient${i}`];
+      const measure = meal[`strMeasure${i}`];
+
+      if (ingredient) {
+        ingredients.push(ingredient + " - " + measure);
+      }
+    }
+  }
+  console.log("Ingredients:", ingredients);
 
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="mt-5 text-2xl font-medium italic">
-        {data?.meals[0].strMeal}
-      </h1>
-      <div className="mt-5 h-96 w-11/12 max-w-4xl overflow-hidden rounded-md">
-        <img
-          src="https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
-          alt=""
-          className="h-full w-full object-cover"
-        />
-      </div>
+    <main className="px-5">
+      {loading && <span>Loading...</span>}
+      {!loading && error && <Error />}
+      {!loading && meal && (
+        <div className="flex flex-col items-center">
+          <h1 className="mt-5 text-2xl font-medium">{meal.strMeal}</h1>
+          <div className="max-w-4x mt-5 aspect-video w-11/12 overflow-hidden rounded-md">
+            <img
+              src="https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
+              alt=""
+              className="h-full w-full object-cover"
+            />
+          </div>
 
-      <div>
-        <h2 className="font-bold">Ingredients</h2>
-        <p>penne rigate</p>
-        <p>olive oil</p>
-        <p>garlic</p>
-      </div>
+          <div className="my-5">
+            <h2 className="text-center text-2xl font-medium italic">
+              Ingredients
+            </h2>
+            <ul className="list-disc">
+              {ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+          </div>
 
-      <div>
-        <h2 className="font-bold">Insructions</h2>
-      </div>
-    </div>
+          <div className="text-center">
+            <h2 className="text-2xl font-medium italic">Instructions</h2>
+            <p>{data?.meals[0].strInstructions}</p>
+          </div>
+        </div>
+      )}
+    </main>
   );
 };
 
